d4b9b59dfb3b457136afba08ad1a22cbc2d5f173
ShirNegev <shirbdc@gmail.com>
2025-03-05 17:53:14 +0200

    added auth middleware

---
+++ src/common/auth_middleware.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+export interface AuthRequest extends Request {
+  user?: { _id: string };
+}
+
+const authMiddleware = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  console.log("auth middleware", req.cookies);
+  const token = req.cookies.access;
+  if (token == null) return res.sendStatus(401);
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.sendStatus(401);
+    req.user = user as { _id: string };
+    next();
+  });
+};
+
+export default authMiddleware;
